3abeb78dcfc9a1a86dad420c2cf3a3f9edc2decb
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2026-01-18 16:12:51 -0300

    create url-shortener project start files

---
+++ short-url-creator/src/main/java/space/rodrigorocha/short_url/short_url_creator/ShortUrlCreatorApplication.java
@@ -0,0 +1,13 @@
+package space.rodrigorocha.short_url.short_url_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUrlCreatorApplication.class, args);
+	}
+
+}
---
+++ short-url-creator/src/test/java/space/rodrigorocha/short_url/short_url_creator/ShortUrlCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package space.rodrigorocha.short_url.short_url_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
