c9e20d0f06968afccae4d5f5342048ebf7d7566f
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2026-04-29 12:15:53 -0300

    feat: create config service

---
+++ config-server/src/test/java/space/rodrigorocha/short_url/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package space.rodrigorocha.short_url.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
